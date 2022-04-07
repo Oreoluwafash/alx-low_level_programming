aa78698120607b2b236045346d32c88ed9003348
Oreoluwafash <oreoluwa.faseyitan@stu.cu.edu.ng>
2022-04-07 15:09:05 -0700

    Low level language

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,4 +12,5 @@ int main(void)
 	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
-}.betty-style.pl 6-size.c
+}
+./betty-style.pl 6-size.c
