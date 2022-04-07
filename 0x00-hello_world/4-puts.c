ddd609f3cfcbf702075a692c2e4c907102129e6e
OreOluwa <101082453+Oreoluwafash@users.noreply.github.com>
2022-04-07 13:39:33 +0100

    Low level language

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#!/bin/bash
-#include<studio.h>
+#include<stdio.h>
 /**
  * main - Entry point
  *
- * Return : Always 0
- * /
- int main(void)
- {puts("\"Programming is like building a multilingual puzzle");
- return (0);
- }
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
