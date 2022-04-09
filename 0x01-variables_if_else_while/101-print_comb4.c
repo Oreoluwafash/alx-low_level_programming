da395f09a8c1d10604932cb0780d7d01e72f8d55
OreOluwa <101082453+Oreoluwafash@users.noreply.github.com>
2022-04-09 12:34:08 +0100

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,34 +6,40 @@
  * Return: Always 0 (Success)
  */
 int main(void)
+	
 {
 	int i, e, g;
 
 	i = 48;
-	e= 48;
-	g= 48;
+	e = 48;
+	g = 48;
+
 	while (e < 58)
 	{
-		g = 48;
-		while ( g < 58)
+		i = 48;
+		while (i < 58)
 		{
-			if (e != i && e != g && i != g && e < i && i < g)
+			g = 48;
+			while (g < 58)
 			{
-				putchar(e);
-				putchar(i);
-				putchar(g);
-				if (i ==56 && e == 55 && g ==57)
+				if (e != i && e != g && i != g && e < i && i < g)
 				{
-					break;
+					putchar(e);
+					putchar(i);
+					putchar(g);
+					if (i == 56 && e == 55 && g == 57)
+					{
+						break;
+					}
+					putchar(',');
+					putchar(' ');
 				}
-				putchar(',');
-				putchar(' ');
+				g++;
 			}
-			g++;
+			i++;
 		}
-		i++;
-	}e++;
-}
-putchar('\n');
-return (0);
+		e++;
+	}
+	putchar('\n');
+	return (0);
 }
