021a96a5bac1882e58140ee3f6ffbd25ffac0247
Oreoluwafash <oreoluwa.faseyitan@stu.cu.edu.ng>
2022-04-13 04:10:49 -0700

    ;

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,9 +12,9 @@ void times_table(void)
 	for (a = 0; a <= 9; a++)
 	{
 		_putchar(48);
-		for (b =1; b <= 9; b++)
+		for (b = 1; b <= 9; b++)
 		{
-			op = a* b;
+			op = a * b;
 			_putchar(44);
 			_putchar(32);
 			if (op <= 9)
