b0af31550847435303cfbf6035a2d304601769eb
Oreoluwafash <oreoluwa.faseyitan@stu.cu.edu.ng>
2022-04-13 03:03:19 -0700

    ;

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit
+ *
+ * Return: Always 0.
+ */
+int print_last_digit(int n)
+{
+	int l;
+
+	l = n % 10;
+	if(l , 0)
+	{
+		_putchar(-l + 48);
+		return (-1);
+	}
+	else
+	{
+		_putchar(l + 48);
+		return (1);
+	}
+}
