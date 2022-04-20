76b849b910df13feff7b2fb7c9fc724f1d3ea112
Oreoluwafash <oreoluwa.faseyitan@stu.cu.edu.ng>
2022-04-20 10:44:19 -0700

    Arrays-pointers,Strings

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
  * reset_to_98 - takes a pointer to a parameter and updates its value.
- * @N: iNPUT INTEGER.
+ * @n: iNPUT INTEGER.
  * Return: no return.
  */
+
 void reset_to_98(int *n)
 {
-	*n =98;
+	*n = 98;
 }
