7f6a848748c34b0a8e49a6a47519770693a7034f
Oreoluwafash <oreoluwa.faseyitan@stu.cu.edu.ng>
2022-04-23 16:15:50 -0700

    Arrays

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * string_toupper - chnges all lowercase letters to upper case
+ * @s: input string
+ * Return: the pointer to destination
+ */
+char *string_toupper(char *s)
+{
+	int count = 0;
+
+	while (*(s + count) != '\0')
+	{
+		if ((*(s + count) > 97) && (*(s + count) <= 122))
+			*(s + count) = *(s + count) - 32;
+		count++;
+	}
+
+	return (s);
+}
