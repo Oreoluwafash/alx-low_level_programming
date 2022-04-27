0451042ba4bff88e74b3edac6d99375981232aac
Oreoluwafash <oreoluwa.faseyitan@stu.cu.edu.ng>
2022-04-27 03:05:44 -0700

    pointers, array, strings

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _strpbrk - searches a string for any of a set of bytes
  * @s: first string
@@ -11,7 +12,7 @@ char *_strpbrk(char *s, char *accept)
 
 	for (i = 0; *(s + i) != '\0'; i++)
 	{
-		for (j = 0; *(accept + j) != '\0'; j++)
+ 		for (j = 0; *(accept + j) != '\0'; j++)
 		{
 			if (*(s + i) == *(accept + j))
 				return (s + i)
